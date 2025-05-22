bc44314607890ce62b9b8f90908f293cd79c7d6d
jurio5 <sia5492@naver.com>
2025-05-23 00:31:16 +0900

    [feat] JDBC 트랜잭션 매니저 구현체를 주입하여 구현

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,65 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_1 {
+
+    //    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            // 비즈니스 로직
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status); // 성공 시 커밋
+        } catch (Exception e) {
+            transactionManager.rollback(status); // 실패 시 롤백
+            throw e;
+        }
+        // 트랜잭션 매니저가 커밋되거나 롤백될 때 자동으로 릴리즈(리소스)를 자동으로 정리해줌 (final 처리 필요X)
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if ("ex".equals(toMember.getMemberId())) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.error("error", e);
+            }
+        }
+    }
+}
