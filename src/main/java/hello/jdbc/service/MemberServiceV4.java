9bcc5cf064ba889dbb6b5b9645496d748ce13ad4
jurio5 <sia5492@naver.com>
2025-05-24 23:51:34 +0900

    [feat] MemberRepository 인터페이스화 및 체크 예외 언체크 예외로 변환

---
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,41 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ * MemberRepository 인터페이스에 의존
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+// 클래스 쪽에 @Transactional 을 붙여도 되는데, 이 경우는 public 으로 되어있는 접근 제한자들만 AOP 적용 대상이 된다.
+public class MemberServiceV4 {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if ("ex".equals(toMember.getMemberId())) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
