8ac07c251c3d673cff5e3570d2a86b7f55029093
jurio5 <sia5492@naver.com>
2025-05-25 00:21:07 +0900

    [feat] 에러 코드를 이용한 체크 예외를 런타임 예외로 변환 처리

---
+++ src/main/java/hello/jdbc/repository/ex/MyDuplicateKeyException.java
@@ -0,0 +1,20 @@
+package hello.jdbc.repository.ex;
+
+public class MyDuplicateKeyException extends MyDbException {
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException() {
+        super();
+    }
+}
